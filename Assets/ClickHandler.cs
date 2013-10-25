7c24c0409178c4850829073f0d4d04f81e2a90bb
Kyle Conroy <kyle@kyleconroy.com>
2013-10-24 17:23:08 -0700

    Initial Commit

---
+++ Assets/ClickHandler.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickHandler : MonoBehaviour {
+	private float fade;
+
+	private Color initialColor;
+	public Simon game;
+	public int label;
+	// Use this for initialization
+	void Start () {
+		fade = 1;
+		initialColor = renderer.material.color;
+	}
+	
+	public void Activate() {
+		fade = 0;
+		audio.Play();
+	}
+	
+	IEnumerator fly() {
+		transform.Translate(Vector3.forward * -5);
+		yield return new WaitForSeconds(1f);
+		transform.Translate(Vector3.forward * 5);
+	}
+	
+	public void Die() {
+		StartCoroutine(fly());
+	}
+	
+	void OnMouseDown() {
+		if (!game.isUserTurn()) {
+			return;
+		}
+		
+		Activate();
+		game.Press(label);
+    }
+	
+	void Update() {
+		
+		// Code for OnMouseDown in the iPhone. Unquote to test.
+		RaycastHit hit = new RaycastHit();
+		
+		for (int i = 0; i < Input.touchCount; ++i) {
+			if (Input.GetTouch(i).phase.Equals(TouchPhase.Began)) {
+				// Construct a ray from the current touch coordinates
+				Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+				if (Physics.Raycast(ray, out hit)) {
+					hit.transform.gameObject.SendMessage("OnMouseDown");
+		      	}
+		  	}
+		}
+		
+		if (fade < 1) {
+			renderer.material.color = Color.Lerp(Color.white, initialColor, fade);
+			fade = fade + Time.deltaTime;
+		}
+	}
+
+}
---
+++ Assets/Simon.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Simon : MonoBehaviour {
+	private List<int> botMoves;
+	private List<int> userMoves;
+	private bool allowUserInput;
+	private GameObject red;
+	private GameObject blue;
+	private GameObject green;
+	private GameObject yellow;
+	
+	public bool isUserTurn() {
+		return allowUserInput;
+	}
+	
+	public GameObject getCube(int label) {
+		switch (label) {
+		case 1:
+			return blue;
+		case 2:
+			return yellow;
+		case 3:
+			return red;
+		case 4:
+			return green;
+		}
+		return null;
+	}
+	
+	public void Press(int label) {
+		userMoves.Add(label);
+		
+		if (userMoves.Count > botMoves.Count) {
+			StartCoroutine(restart());
+			return;
+		}
+		
+		for (int i = 0; i < userMoves.Count; i++) {
+			if (userMoves[i] != botMoves[i]) {
+				StartCoroutine(restart());
+				return;
+			}
+        }
+		
+		if (userMoves.Count == botMoves.Count) {
+			allowUserInput = false;
+			StartCoroutine(play());
+		}
+	}
+	
+	IEnumerator restart() {
+		allowUserInput = false;
+		userMoves.Clear();
+		botMoves.Clear();
+		
+		yield return new WaitForSeconds(.5f);
+		audio.Play();
+		
+		blue.SendMessage("Die");
+		yield return new WaitForSeconds(.2f);
+		
+		yellow.SendMessage("Die");
+		yield return new WaitForSeconds(.2f);
+		
+		red.SendMessage("Die");
+		yield return new WaitForSeconds(.2f);
+		
+		green.SendMessage("Die");
+		yield return new WaitForSeconds(.2f);
+		
+		yield return new WaitForSeconds(2f);
+				
+		StartCoroutine(play());
+	}
+	
+	   // Define the schedule of events.
+	IEnumerator play() {
+		yield return new WaitForSeconds(1f);
+		
+		botMoves.Add(Random.Range(1, 5));
+		
+		foreach (int label in botMoves) {
+			GameObject cube = getCube(label);
+			cube.SendMessage("Activate");
+			yield return new WaitForSeconds(.5f);
+		}
+	
+		userMoves.Clear();
+		allowUserInput = true;
+   	}
+	
+	// Use this for initialization
+	void Start () {
+		botMoves = new List<int>();
+		userMoves = new List<int>();
+
+		red = GameObject.Find("redCube");
+		blue = GameObject.Find("blueCube");
+		green = GameObject.Find("greenCube");
+		yellow = GameObject.Find("yellowCube");
+
+		allowUserInput = false;
+		StartCoroutine(play());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}	
+}
